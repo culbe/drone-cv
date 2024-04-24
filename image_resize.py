a614a18ef4b4fdc50ee76f120736cf8fd27017ac
Matthias Culbertson <matthias.culbertson@gmail.com>
2024-04-24 13:43:39 -0400

    added resized images

---
+++ image_resize.py
@@ -0,0 +1,23 @@
+import os
+import csv
+import numpy as np
+from PIL import Image
+
+def process_files(input_folder, output_folder):
+   
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for filename in os.listdir(input_folder):        
+        if filename.endswith('.jpg'):  
+            input_file_path = os.path.join(input_folder, filename)
+            output_file_path = os.path.join(output_folder, filename)
+
+            output_image = Image.open(input_file_path).resize((640,640))
+
+            output_image.save(output_file_path)
+
+input_folder = 'datasets/images/val'
+output_folder = 'datasets/images_640/val'
+process_files(input_folder, output_folder)
